b95c29780e631d20b68f066c30746b3b69f4af39
MalejaRodri <rodriguez_malejandra@javeriana.edu.co>
2026-09-06 00:33:35 -0500

    Borrador TipoAtributo (#119, #120)
    Add comments for clarification on attribute classification and database status.

--- app/Entrega1/wisetrip_entrega1/src/main/java/com/wisetrip/modelo/TipoAtributo.java
+++ app/Entrega1/wisetrip_entrega1/src/main/java/com/wisetrip/modelo/TipoAtributo.java
@@ -1,3 +1,7 @@
+//esto tambien hay que editarlo bien porque el sistema 
+//se confunde entre las busquedas de la API y los atributospor ciudad manuales
+//Clasificación de atributos (manual/automático/perfil)
+//Ademas aun no esta en la base de datos 
 package com.wisetrip.modelo;
 
 public enum TipoAtributo {
